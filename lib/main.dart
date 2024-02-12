206169e6ed9765261ae342f201a157d8735497a3
Zignuts Technolab <ztlab77@Zignutss-Mac-mini.local>
2024-02-12 18:39:25 +0530

    add download feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'dart:convert';
-import 'package:flutter/cupertino.dart';
+import 'dart:io';
+// import 'dart:html';
 import 'package:flutter/material.dart';
+import 'package:flutter_file_dialog/flutter_file_dialog.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:path_provider/path_provider.dart';
 
-main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize(
+    debug: true, // optional: set false to disable printing logs to console
+  );
   runApp(wallpaperApp());
 }
 
 class wallpaperApp extends StatefulWidget {
-  const wallpaperApp({super.key});
+  const wallpaperApp({Key? key}) : super(key: key);
 
   @override
   State<wallpaperApp> createState() => _wallpaperAppState();
@@ -17,12 +25,13 @@ class wallpaperApp extends StatefulWidget {
 class _wallpaperAppState extends State<wallpaperApp> {
   List data = [];
 
+  @override
   void initState() {
     super.initState();
     getData();
   }
 
-  getData() async {
+  Future<void> getData() async {
     var imgData = await http.get(Uri.parse(
         'https://api.unsplash.com/photos?page=1&client_id=VHFW5Her3S17W9z06s8n7Ll_1Ka8PVbRCeAWHVIKZeo'));
     var jData = jsonDecode(imgData.body);
@@ -31,10 +40,26 @@ class _wallpaperAppState extends State<wallpaperApp> {
     });
   }
 
+  saveimage(String  url) async {
+    var response = await http.get(Uri.parse(url));
+    // Get temporary directory
+    final dir = await getTemporaryDirectory();
+    // Create an image name
+    var filename = '${dir.path}/image.png';
+    // Save to filesystem
+    final file = File(filename);
+    await file.writeAsBytes(response.bodyBytes);
+    // Ask the user to save it
+    final params = SaveFileDialogParams(sourceFilePath: file.path);
+    final finalPath = await FlutterFileDialog.saveFile(params: params);
+  }
+
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.black,
@@ -44,19 +69,34 @@ class _wallpaperAppState extends State<wallpaperApp> {
           backgroundColor: Colors.amberAccent,
         ),
         body: GridView.builder(
-                      itemCount: data.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2),
-            itemBuilder: (context, index) {
-              return Container(
-                height: 400,
-                width: double.infinity,
-                padding: const EdgeInsets.all(5),
-                child: Image.network(
-                  data[index]['urls']['small'],
-                ),
-              );
-            }),
+          itemCount: data.length,
+          gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                // Handle onTap event if needed
+              },
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Image.network(
+                      data[index]['urls']['small'],
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      saveimage(data[index]['urls']['full']);
+                    },
+                    child: Text('Download'),
+                  ),
+
+                ],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
